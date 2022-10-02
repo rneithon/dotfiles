c42bcf166bff3da9cf21a05434fd2d81278b8a36
MeiWagatsuma <wagamei256@gmail.com>
2022-10-02 11:38:02 +0900

    Change escape key to jk

--- nvim/settings/plugin/better-escape.lua
+++ nvim/settings/plugin/better-escape.lua
@@ -1,11 +1,6 @@
--- lua, default settings
 require("better_escape").setup {
-    mapping = {"jf"}, -- a table with mappings to use
-    timeout = 100, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+    mapping = { "jk" }, -- a table with mappings to use
+    timeout = 200, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
     clear_empty_lines = false, -- clear line after escaping if there is only whitespace
     keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
-    -- example(recommended)
-    -- keys = function()
-    --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-    -- end,
 }
