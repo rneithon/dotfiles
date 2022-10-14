4d632d33b692cfdc5d92430a5b6d958d862cb315
MeiWagatsuma <wagamei256@gmail.com>
2022-10-14 11:30:50 +0900

    Improve format configuration
    Add async formatting
    Add astro in filetype of prettier

--- nvim/settings/lsp/lspconfig.lua
+++ nvim/settings/lsp/lspconfig.lua
@@ -22,7 +22,11 @@ local null_sources = {}
 for _, package in ipairs(mason_registry.get_installed_packages()) do
 	local package_categories = package.spec.categories[1]
 	if package_categories == mason_package.Cat.Formatter then
-		table.insert(null_sources, null_ls.builtins.formatting[package.name])
+		if package.name == "prettier" then
+			table.insert(null_sources, null_ls.builtins.formatting.prettier.with({ extra_filetypes = { "astro" } }))
+		else
+			table.insert(null_sources, null_ls.builtins.formatting[package.name])
+		end
 	end
 	if package_categories == mason_package.Cat.Linter then
 		table.insert(
@@ -34,32 +38,39 @@ for _, package in ipairs(mason_registry.get_installed_packages()) do
 	end
 end
 
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
+local async_formatting = function(bufnr)
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+	vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		vim.lsp.util.make_formatting_params({}),
+		function(err, res, ctx)
+			if err then
+				local err_msg = type(err) == "string" and err or err.message
+				-- you can modify the log message / level (or ignore it completely)
+				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+				return
+			end
+
+			-- don't apply results if buffer is unloaded or has been modified
+			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+				return
+			end
+
+			if res then
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+				vim.api.nvim_buf_call(bufnr, function()
+					vim.cmd("silent noautocmd update")
+				end)
+			end
+		end
+	)
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-	-- if client.supports_method("textDocument/formatting") then
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				lsp_formatting(bufnr)
-			end,
-		})
-	end
-end
 
 null_ls.setup({
 	on_attach = function(client, bufnr)
@@ -70,7 +81,7 @@ null_ls.setup({
 				group = augroup,
 				buffer = bufnr,
 				callback = function()
-					lsp_formatting(bufnr)
+					async_formatting(bufnr)
 				end,
 			})
 		end
@@ -81,7 +92,7 @@ null_ls.setup({
 local status, lspconfig = pcall(require, "mason-lspconfig")
 if not status then
 	return
-end
+
 lspconfig.setup_handlers({
 	function(server) -- default handler (optional)
 		local opt = {
