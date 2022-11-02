e4beecdeb14e46984ceb9ca40746286f0aff68dd
MeiWagatsuma <wagamei256@gmail.com>
2022-11-02 19:33:18 +0900

    To automatically connect lsp to null-ls

---
+++ nvim/settings/lsp/formater-and-linter.lua
@@ -0,0 +1,103 @@
+local status, mason_null_ls = pcall(require, "mason-null-ls")
+---
+-- mason setting
+---
+if not status then
+	print("mason-null-ls is not found")
+end
+
+local formatter = {
+	"prettier",
+	"stylua",
+	"goimports",
+}
+
+local linter = {
+	"eslint_d",
+	"luacheck",
+	"revive",
+}
+
+-- Merge formatter and linter into one table
+local formatter_linter = formatter
+for I = 1, #linter do
+	formatter_linter[#formatter + I] = linter[I]
+end
+
+mason_null_ls.setup({
+	ensure_installed = formatter_linter,
+})
+---
+-- null_ls setting
+---
+local null_ls_status, null_ls = pcall(require, "null-ls")
+if not null_ls_status then
+	print("null-ls is not found")
+end
+
+local null_sources = function()
+	local source_return = {}
+
+	-- Set the formatters to null-ls
+	for _, package in ipairs(formatter) do
+		table.insert(source_return, null_ls.builtins.formatting[package])
+	end
+
+	-- Set the diagnostics to null-ls
+	for _, package in ipairs(linter) do
+		table.insert(source_return, null_ls.builtins.diagnostics[package])
+	end
+	return source_return
+end
+
+-- auto format setting
+local async_formatting = function(bufnr)
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+	vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		vim.lsp.util.make_formatting_params({}),
+		function(err, res, ctx)
+			if err then
+				local err_msg = type(err) == "string" and err or err.message
+				-- you can modify the log message / level (or ignore it completely)
+				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+				return
+			end
+
+			-- don't apply results if buffer is unloaded or has been modified
+			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+				return
+			end
+
+			if res then
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+				vim.api.nvim_buf_call(bufnr, function()
+					vim.cmd("silent noautocmd update")
+				end)
+			end
+		end
+	)
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+	on_attach = function(client, bufnr)
+		-- if client.supports_method("textDocument/formatting") then
+		if client.server_capabilities.documentFormattingProvider then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					async_formatting(bufnr)
+				end,
+			})
+		end
+	end,
+	sources = null_sources(),
+})
--- nvim/settings/lsp/lspconfig.lua
+++ nvim/settings/lsp/lspconfig.lua
@@ -2,6 +2,7 @@ local status, mason = pcall(require, "mason")
 if not status then
 	return
 end
+
 mason.setup({
 	ui = {
 		icons = {
@@ -10,83 +11,12 @@ mason.setup({
 			package_uninstalled = "✗",
 		},
 	},
-})
-
-local null_ls = require("null-ls")
-
-local mason_package = require("mason-core.package")
-local mason_registry = require("mason-registry")
-
-local null_sources = {}
-
-for _, package in ipairs(mason_registry.get_installed_packages()) do
-	local package_categories = package.spec.categories[1]
-	if package_categories == mason_package.Cat.Formatter then
-		if package.name == "prettier" then
-			table.insert(null_sources, null_ls.builtins.formatting.prettier.with({ extra_filetypes = { "astro" } }))
-		else
-			table.insert(null_sources, null_ls.builtins.formatting[package.name])
-		end
-	end
-	if package_categories == mason_package.Cat.Linter then
-		table.insert(
-			null_sources,
-			null_ls.builtins.diagnostics[package.name].with({
-				"[" .. package.name .. "] #{m}\n(#{c})",
-			})
-		)
-	end
-end
-
-local async_formatting = function(bufnr)
-	bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-	vim.lsp.buf_request(
-		bufnr,
-		"textDocument/formatting",
-		vim.lsp.util.make_formatting_params({}),
-		function(err, res, ctx)
-			if err then
-				local err_msg = type(err) == "string" and err or err.message
-				-- you can modify the log message / level (or ignore it completely)
-				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-				return
-			end
-
-			-- don't apply results if buffer is unloaded or has been modified
-			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-				return
-			end
-
-			if res then
-				local client = vim.lsp.get_client_by_id(ctx.client_id)
-				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-				vim.api.nvim_buf_call(bufnr, function()
-					vim.cmd("silent noautocmd update")
-				end)
-			end
-		end
-	)
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-null_ls.setup({
-	on_attach = function(client, bufnr)
-		-- if client.supports_method("textDocument/formatting") then
-		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					async_formatting(bufnr)
-				end,
-			})
-		end
-	end,
-	sources = null_sources,
+	ensure_installed = {
+		"astro-language-server",
+		"gopls",
+		"lua-language-server",
+		"typescript-language-server",
+	},
 })
 
 local status, lspconfig = pcall(require, "mason-lspconfig")
@@ -106,9 +36,9 @@ lspconfig.setup_handlers({
 			capabilities = require("cmp_nvim_lsp").default_capabilities(),
 			settings = {
 				Lua = {
-					diagnostics = { globals = {'vim'} }
-				}
-			}
+					diagnostics = { globals = { "vim" } },
+				},
+			},
 		}
 		require("lspconfig")[server].setup(opt)
 	end,
