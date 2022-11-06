90060c2031894e47dc6d38a45a9363753761997c
MeiWagatsuma <wagamei256@gmail.com>
2022-11-06 16:10:40 +0900

    Added a subcommand named tree to git

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,6 +4,15 @@ alias la="lsd -l -a"
 alias ..="cd .."
 alias ...="cd ../../"
 
+# Add a subcommand named tree to git
+git() {
+    if [[ $@ == "tree" ]]; then
+        command git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
+    else
+        command git "$@"
+    fi
+}
+
 alias fga="fzf-git-add"
 alias fd='fzf-cdr'
 alias fda='fzf-cdr-all'
