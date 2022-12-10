84a265f073573ddd50bce4d326750393ec66287c
MeiWagatsuma <wagamei256@gmail.com>
2022-12-10 15:42:55 +0900

    Added some options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,6 +1,17 @@
 local function load_options()
   local global_local = {
+    termguicolors = true,
     relativenumber = true,
+    mouse = "a",
+
+    expandtab = true,
+    tabstop = 2,
+    shiftwidht = 2,
+    softtabstop = 2,
+
+    smartindent = true,
+    undodir = "$HOME/.vim/undodir",
+    cursorline = true,
   }
   for name, value in pairs(global_local) do
     vim.o[name] = value
