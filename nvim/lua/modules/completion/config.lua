29d2cdd8b092fe1105c12ee6c7f082be93719eb0
MeiWagatsuma <wagamei256@gmail.com>
2022-12-17 14:22:18 +0900

    Change formatting config

--- nvim/lua/modules/completion/config.lua
+++ nvim/lua/modules/completion/config.lua
@@ -30,38 +30,6 @@ function config.null_ls()
     return source_return
   end
 
-  -- auto format setting
-  local async_formatting = function(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-    vim.lsp.buf_request(
-      bufnr,
-      "textDocument/formatting",
-      vim.lsp.util.make_formatting_params({}),
-      function(err, res, ctx)
-        if err then
-          local err_msg = type(err) == "string" and err or err.message
-          -- you can modify the log message / level (or ignore it completely)
-          vim.notify("formatting: " .. err_msg, vim.log.levels.warn)
-          return
-        end
-
-        -- don't apply results if buffer is unloaded or has been modified
-        if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-          return
-        end
-
-        if res then
-          local client = vim.lsp.get_client_by_id(ctx.client_id)
-          vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-          vim.api.nvim_buf_call(bufnr, function()
-            vim.cmd("silent noautocmd update")
-          end)
-        end
-      end
-    )
-  end
-
   -- if you want to set up formatting on save, you can use this as a callback
   local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
@@ -73,7 +41,13 @@ function config.null_ls()
           group = augroup,
           buffer = bufnr,
           callback = function()
-            async_formatting(bufnr)
+            vim.lsp.buf.format({
+              bufnr = bufnr,
+              async = true,
+              filter = function(client)
+                return client.name == "null-ls"
+              end,
+            })
             vim.notify("formatted")
           end,
         })
