c6e3bfc3f923da3b32038bf95a57dce205a47bd2
MeiWagatsuma <wagamei256@gmail.com>
2023-02-09 17:29:21 +0900

    Add image viewer

--- nvim/lua/modules/editor.lua
+++ nvim/lua/modules/editor.lua
@@ -1,5 +1,24 @@
 return {
 	{
+		"samodostal/image.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "m00qek/baleia.nvim" },
+		event = "BufReadPre",
+		config = function()
+			require("image").setup({
+				render = {
+					min_padding = 5,
+					show_label = true,
+					use_dither = true,
+					foreground_color = true,
+					background_color = true,
+				},
+				events = {
+					update_on_nvim_resize = true,
+				},
+			})
+		end,
+	},
+	{
 		"kazhala/close-buffers.nvim",
 		config = true,
 	},
