e37e1c2d4b347535d8f2e3b4f96e7d29a2a7edc2
MeiWagatsuma <wagamei256@gmail.com>
2023-02-25 12:38:43 +0900

    Add Automatic adding brackets to cmp

--- nvim/lua/modules/completion.lua
+++ nvim/lua/modules/completion.lua
@@ -212,6 +212,7 @@ return {
 		},
 		config = function()
 			local cmp = require("cmp")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 			local lspkind = require("lspkind")
 			local luasnip = require("luasnip")
 			local compare = require("cmp.config.compare")
@@ -220,15 +221,7 @@ return {
 				delete_check_events = "TextChanged,InsertEnter",
 			})
 
-			local feedkey = function(key, mode)
-				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-			end
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 			cmp.setup({
 				preselect = cmp.PreselectMode.None,
