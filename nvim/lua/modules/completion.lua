b1a069dd06dd8dcf0c0da2d6cef1415981543f2d
MeiWagatsuma <wagamei256@gmail.com>
2023-02-25 12:59:51 +0900

    be faster to show cmp menu speed

--- nvim/lua/modules/completion.lua
+++ nvim/lua/modules/completion.lua
@@ -223,6 +223,27 @@ return {
 
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
+			vim.api.nvim_create_autocmd({ "TextChangedI", "TextChangedP" }, {
+				callback = function()
+					local line = vim.api.nvim_get_current_line()
+					local cursor = vim.api.nvim_win_get_cursor(0)[2]
+
+					local current = string.sub(line, cursor, cursor + 1)
+					if current == "." or current == "," or current == " " then
+						require("cmp").close()
+					end
+
+					local before_line = string.sub(line, 1, cursor + 1)
+					local after_line = string.sub(line, cursor + 1, -1)
+					if not string.match(before_line, "^%s+$") then
+						if after_line == "" or string.match(before_line, " $") or string.match(before_line, "%.$") then
+							require("cmp").complete()
+						end
+					end
+				end,
+				pattern = "*",
+			})
+
 			cmp.setup({
 				preselect = cmp.PreselectMode.None,
 				snippet = {
