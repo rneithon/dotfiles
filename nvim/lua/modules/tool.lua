548c9199d1762aff32f5a4aeabd011f5f5c96e98
MeiWagatsuma <wagamei256@gmail.com>
2023-02-25 12:19:03 +0900

    Add tag of nvim-window-picker

--- nvim/lua/modules/tool.lua
+++ nvim/lua/modules/tool.lua
@@ -250,7 +250,7 @@ return {
 			{
 				-- only needed if you want to use the commands with "_with_window_picker" suffix
 				"s1n7ax/nvim-window-picker",
-				tag = "v1.*",
+				tag = "v1.5",
 				config = function()
 					require("window-picker").setup({
 						autoselect_one = true,
