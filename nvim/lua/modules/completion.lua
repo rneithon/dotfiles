a198bc02e1b81358ba64751113125c97086d7d46
MeiWagatsuma <wagamei256@gmail.com>
2023-02-27 15:55:50 +0900

    Delete unnecessary  function

--- nvim/lua/modules/completion.lua
+++ nvim/lua/modules/completion.lua
@@ -224,33 +224,6 @@ return {
 
 			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-			vim.api.nvim_create_autocmd({ "TextChangedI", "TextChangedP" }, {
-				callback = function()
-					-- Disable in telescope
-					local buftype = vim.api.nvim_buf_get_option(0, "buftype")
-					if buftype == "prompt" then
-						return
-					end
-
-					local line = vim.api.nvim_get_current_line()
-					local cursor = vim.api.nvim_win_get_cursor(0)[2]
-
-					local current = string.sub(line, cursor, cursor + 1)
-					if current == "." or current == "," or current == " " then
-						require("cmp").close()
-					end
-
-					local before_line = string.sub(line, 1, cursor + 1)
-					local after_line = string.sub(line, cursor + 1, -1)
-					if not string.match(before_line, "^%s+$") then
-						if after_line == "" or string.match(before_line, " $") or string.match(before_line, "%.$") then
-							require("cmp").complete()
-						end
-					end
-				end,
-				pattern = "*",
-			})
-
 			local compare = require("cmp.config.compare")
 			compare.lsp_scores = function(entry1, entry2)
 				local diff
