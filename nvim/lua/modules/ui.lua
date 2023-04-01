2adadc764412ef45327eca9b9671a604c0f49f9c
MeiWagatsuma <wagamei256@gmail.com>
2023-04-01 20:02:22 +0900

    Remove unused config from transparent.nvim

--- nvim/lua/modules/ui.lua
+++ nvim/lua/modules/ui.lua
@@ -3,7 +3,6 @@ return {
 		"xiyaowong/transparent.nvim",
 		config = function()
 			require("transparent").setup({
-				enable = true, -- boolean: enable transparent
 				extra_groups = { -- table/string: additional groups that should be cleared
 					-- In particular, when you set it to 'all', that means all available groups
 
@@ -15,8 +14,6 @@ return {
 					"BufferLineSeparator",
 					"BufferLineIndicatorSelected",
 				},
-				exclude = {}, -- table: groups you don't want to clear
-				ignore_linked_group = true, -- boolean: don't clear a group that links to another group
 			})
 		end,
 	},
