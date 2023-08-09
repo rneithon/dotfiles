ff58234cff9adc1136888cd14bc7e58f25d413d0
MeiWagatsuma <wagamei256@gmail.com>
2023-08-09 16:50:45 +0900

    lazy load of zinit

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -3,12 +3,12 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
+# 非同期処理できるようになる
+zinit load mafredri/zsh-async
+
 # vim mode
 zinit ice depth"1"
 zinit light jeffreytse/zsh-vi-mode
-
-# 非同期処理できるようになる
-zinit load mafredri/zsh-async
 
 zinit ice depth"1"
 zinit light romkatv/powerlevel10k
@@ -17,7 +17,7 @@ zinit load agkozak/zsh-z
 # 過去に入力したコマンドの履歴が灰色のサジェストで出る
 zinit load zsh-users/zsh-autosuggestions
 # 補完強化
-zinit load zsh-users/zsh-completions
+zinit wait'0' lucid light-mode for zsh-users/zsh-completions
 # 256色表示にする
 zinit load chrissicool/zsh-256color
 # 構文のハイライト(https://github.com/zsh-users/zsh-syntax-highlighting)
@@ -27,9 +27,7 @@ zinit load BurntSushi/ripgrep
 
 zinit load junegunn/fzf
 
-zinit light Aloxaf/fzf-tab
-
-zinit load zdharma-continuum/fast-syntax-highlighting
+zinit wait'0' lucid light-mode for zdharma-continuum/fast-syntax-highlighting
 
 # For postponing loading `fzf`
 zinit ice lucid wait
