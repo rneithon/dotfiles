28281eb1b08c3a42cda93b21b8afa530fb3ca968
MeiWagatsuma <wagamei256@gmail.com>
2023-10-23 12:39:45 +0900

    Add LazyVim

---
+++ config/LazyVim/lua/config/keymaps.lua
@@ -0,0 +1,23 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+map({ "n", "v" }, "x", '"_x"', { noremap = true })
+
+map("n", "<CR>", "i<CR><ESC>", { noremap = true })
+
+map("i", "<C-f>", "<Right>", { noremap = true })
+map("i", "<C-b>", "<Left>", { noremap = true })
+map("i", "<C-n>", "<Down>", { noremap = true })
+map("i", "<C-p>", "<Up>", { noremap = true })
+
+-- Terminal
+map("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
+map("t", "<C-q>", "<C-:><C-n>:q<cr>", { noremap = true })
+map(
+  "n",
+  "gh",
+  "<cmd>Gitsigns toggle_deleted<CR>:Gitsigns toggle_numhl<CR>:Gitsigns toggle_linehl<cr>",
+  { desc = "Show git diff" }
+)
---
+++ config/LazyVim/lua/plugins/example.lua
@@ -0,0 +1,358 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+local map = vim.keymap.set
+return {
+  {
+    "kosayoda/nvim-lightbulb",
+    config = function()
+      require("nvim-lightbulb").setup({
+        autocmd = { enabled = true },
+      })
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { use_diagnostic_signs = false },
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    opt = {},
+    config = function(_, opts)
+      require("lspsaga").setup(opts)
+    end,
+    cmd = "Lspsaga",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
+    keys = {
+      {
+        "[e",
+        function()
+          require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+        end,
+        desc = "Go to previous error",
+      },
+      {
+        "]e",
+        function()
+          require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+        end,
+        desc = "Go to prev error",
+      },
+      {
+        "[d",
+        function()
+          require("lspsaga.diagnostic"):goto_prev()
+        end,
+        desc = "Go to prev diagnostic",
+      },
+      {
+        "]d",
+        function()
+          require("lspsaga.diagnostic"):goto_next()
+        end,
+        desc = "Go to next diagnostic",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        playground = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  {
+    "ziontee113/syntax-tree-surfer",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    keys = {
+      { "vi", "<cmd>STSSelectCurrentNode<cr>", mode = "n", desc = "Select Current Node" },
+      { "va", "<cmd>STSSelectMasterNode<cr>", mode = "n", desc = "Select Master Node" },
+      { "N", "<cmd>STSSelectNextSiblingNode<cr>", mode = "x", desc = "Select Next Sibling Node" },
+      { "P", "<cmd>STSSelectPrevSiblingNode<cr>", mode = "x", desc = "Select Previous Sibling Node" },
+      { "K", "<cmd>STSSelectParentNode<cr>", mode = "x", desc = "Select Parent Node" },
+      { "J", "<cmd>STSSelectChildNode<cr>", mode = "x", desc = "Select Child Node" },
+    },
+    config = function()
+      require("syntax-tree-surfer").setup()
+    end,
+  },
+  {
+    "kiyoon/treesitter-indent-object.nvim",
+    keys = {
+      {
+        "ai",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_outer()
+        end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer)",
+      },
+      {
+        "aI",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_outer(true)
+        end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (outer, line-wise)",
+      },
+      {
+        "ii",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_inner()
+        end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, partial range)",
+      },
+      {
+        "iI",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_inner(true, "V")
+        end,
+        mode = { "x", "o" },
+        desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
+      },
+    },
+  },
+  -- Disable code context
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    enabled = false,
+  },
+  -- Disable indent line
+  {
+    "echasnovski/mini.indentscope",
+    enabled = false,
+  },
+  "echasnovski/mini.indentscope",
+  -- Replace command UI
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+  {
+    "gelguy/wilder.nvim",
+    dependencies = {
+      { "romgrk/fzy-lua-native", build = "make" },
+    },
+    config = function()
+      local wilder = require("wilder")
+      wilder.setup({ modes = { ":", "/", "?" } })
+      -- Disable Python remote plugin
+      wilder.set_option("use_python_remote_plugin", 0)
+
+      wilder.set_option("pipeline", {
+        wilder.branch(
+          wilder.cmdline_pipeline({
+            fuzzy = 1,
+            fuzzy_filter = wilder.lua_fzy_filter(),
+          }),
+          wilder.search_pipeline()
+        ),
+      })
+
+      wilder.set_option(
+        "renderer",
+        wilder.popupmenu_renderer({
+          highlighter = {
+            wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
+            -- at https://github.com/romgrk/fzy-lua-native
+          },
+          highlights = {
+            accent = wilder.make_hl("WilderAccent", "Pmenu", {
+              { a = 1 },
+              { a = 1 },
+              { foreground = "#f4468f" },
+            }),
+          },
+          left = { " ", wilder.popupmenu_devicons() },
+          right = { " ", wilder.popupmenu_scrollbar() },
+        })
+      )
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    opts = {},
+    config = function(opts)
+      require("neogit").setup(opts)
+      map("n", "<space>gs", "<cmd>Neogit<cr>", { desc = "Open git tool interface" })
+    end,
+    keys = {
+      { "<leader>gs", "<cmd>Neogit<cr>", desc = "Open git tool interface" },
+    },
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup({
+        extra_groups = { -- table/string: additional groups that should be cleared
+          -- In particular, when you set it to 'all', that means all available groups
+
+          -- example of akinsho/nvim-bufferline.lua
+          "BufferLineTabClose",
+          "BufferlineBufferSelected",
+          "BufferLineFill",
+          "BufferLineBackground",
+          "BufferLineSeparator",
+          "BufferLineIndicatorSelected",
+        },
+      })
+    end,
+  },
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    keys = { { "<C-n>", mode = { "v", "i", "n" } } },
+    config = function() end,
+    enabled = not vim.g.vscode,
+  },
+  {
+    "chaoren/vim-wordmotion",
+    keys = {
+      { "w", mode = { "n", "v" } },
+      { "e", mode = { "n", "v" } },
+      { "b", mode = { "n", "v" } },
+      { "W", mode = { "n", "v" } },
+      { "E", mode = { "n", "v" } },
+      { "B", mode = { "n", "v" } },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = function(_, opts)
+      -- table.insert(opts.options, { show_close_icon = false })
+
+      -- opts.options.show_close_icon = false
+      opts.options = vim.tbl_extend("force", opts.options, {
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        always_show_bufferline = true,
+        indicator = {
+          icon = "▎", -- this should be omitted if indicator style is not 'icon'
+          style = "underline",
+        },
+      })
+    end,
+  },
+
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fP",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").git_files()
+        end,
+        desc = "Find Git File",
+      },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+      },
+    },
+  },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  -- then: setup supertab in cmp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-e>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.abort()
+          else
+            cmp.complete()
+          end
+        end),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({
+              behavior = cmp.ConfirmBehavior.Replace,
+              select = true,
+            })
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
